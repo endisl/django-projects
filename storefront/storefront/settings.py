f56f228140b3e0f59242ac1dbb35f88382e2ea62
endisl <endiluamba@gmail.com>
2022-07-12 23:24:37 -0300

    store

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -33,10 +33,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.contenttypes',    
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playground'
 ]
 
 MIDDLEWARE = [
