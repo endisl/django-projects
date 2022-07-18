5256f82e328fb77a07df2d8cac033cd8a9554a83
endisl <endiluamba@gmail.com>
2022-07-17 23:54:27 -0300

    djvs

--- django-vscode/hello/views.py
+++ django-vscode/hello/views.py
@@ -1,4 +1,4 @@
-import re
+from django.shortcuts import render
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 
@@ -8,19 +8,11 @@ def home(request):
 
 
 def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%a, %d %b, %y at %X")
-
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-
-    return HttpResponse(content)
-
-
-print("http://127.0.0.1:8000/hello/VSCode")
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
