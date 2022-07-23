e254a89f5a68ad2812f29cd0e8253c0ac1d23c3b
endisl <endiluamba@gmail.com>
2022-07-23 00:11:19 -0300

    djvs

--- django-vscode/hello/urls.py
+++ django-vscode/hello/urls.py
@@ -1,8 +1,16 @@
+import imp
 from django.urls import path
 from hello import views
+from hello.models import LogMessage
+
+home_list_view = views.HomeListView.as_view(
+    queryset=LogMessage.objects.order_by("-log_date")[:5],
+    context_object_name="message_list",
+    template_name="hello/home.html",
+)
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", home_list_view, name="home"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("log/", views.log_message, name="log"),
--- django-vscode/hello/views.py
+++ django-vscode/hello/views.py
@@ -1,13 +1,17 @@
-import imp
 from django.shortcuts import render, redirect
 from django.utils.timezone import datetime
-from django.http import HttpResponse
+from django.views.generic import ListView
 from hello.forms import LogMessageForm
 from hello.models import LogMessage
 
 
-def home(request):
-    return render(request, "hello/home.html")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 
 def about(request):
