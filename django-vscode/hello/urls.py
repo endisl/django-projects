97868d17cda7781313cbf6e00af85eb7805fcc12
endisl <endiluamba@gmail.com>
2022-07-22 23:44:37 -0300

    djvs

--- django-vscode/hello/urls.py
+++ django-vscode/hello/urls.py
@@ -3,7 +3,7 @@ from hello import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    #path("hello/<name>", views.hello_there, name="hello_there"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("log/", views.log_message, name="log"),
 ]
--- django-vscode/hello/views.py
+++ django-vscode/hello/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+import imp
+from django.shortcuts import render, redirect
 from django.utils.timezone import datetime
 from django.http import HttpResponse
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
 
 def home(request):
@@ -15,12 +18,14 @@ def contact(request):
     return render(request, "hello/contact.html")
 
 
-""" def hello_there(request, name):
-    return render(
-        request,
-        'hello/hello_there.html',
-        {
-            'name': name,
-            'date': datetime.now()
-        }
-    ) """
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
