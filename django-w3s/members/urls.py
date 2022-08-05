9b69e64b2d249c23ecceafdc4c2596a7a47ac640
endisl <endiluamba@gmail.com>
2022-08-04 22:16:08 -0300

    w3s

--- django-w3s/members/urls.py
+++ django-w3s/members/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
-    path('add/addrecord/', views.addrecord, name='addrecord')
+    path('add/addrecord/', views.addrecord, name='addrecord'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),
+    path('update/updaterecord/<int:id>',
+         views.updaterecord, name='updaterecord'),
 ]
--- django-w3s/members/views.py
+++ django-w3s/members/views.py
@@ -27,3 +27,30 @@ def addrecord(request):
     member.save()
 
     return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request, id):
+    member = Members.objects.get(id=id)
+    member.delete()
+
+    return HttpResponseRedirect(reverse('index'))
+
+
+def update(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+        'mymember': mymember
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def updaterecord(request, id):
+    first = request.POST['first']
+    last = request.POST['last']
+    member = Members.objects.get(id=id)
+    member.firstname = first
+    member.lastname = last
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
