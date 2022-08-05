03f5ed92f4638e813a9a0dbc1bf2f2ecf2b73139
endisl <endiluamba@gmail.com>
2022-08-04 21:55:13 -0300

    w3s

--- django-w3s/members/views.py
+++ django-w3s/members/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Members
 
 
@@ -11,3 +12,18 @@ def index(request):
     }
 
     return HttpResponse(template.render(context, request))
+
+
+def add(request):
+    template = loader.get_template('add.html')
+
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+
+    return HttpResponseRedirect(reverse('index'))
