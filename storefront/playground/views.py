8c98bbd25422c7acd5676b067b333abea674927b
endisl <endiluamba@gmail.com>
2022-08-08 23:58:52 -0300

    store

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    product = Product.objects.get(pk=1)
+    try:
+        product = Product.objects.get(pk=0)
+    except ObjectDoesNotExist:
+        pass
 
     return render(request, 'hello.html', {'name': 'John'})
