abbe49c9b842c6a830595cea4787b3e5db42648d
endisl <endiluamba@gmail.com>
2022-08-08 23:51:47 -0300

    store

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 
 def say_hello(request):
+    product = Product.objects.get(pk=1)
+
     return render(request, 'hello.html', {'name': 'Mosh'})
