98fb728cd0d1ae1ae6879bc9a8d965cf9c3552f7
endisl <endiluamba@gmail.com>
2022-08-11 00:05:05 -0300

    store

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -5,18 +5,18 @@ from store.models import Collection, Customer, Order, OrderItem, Product
 
 def say_hello(request):
     # Customers with .com accounts
-    query_set = Customer.objects.filter(email__contains='.com')
+    # query_set = Customer.objects.filter(email__icontains='.com')
 
     # Collections that don't have a featured product
-    #query_set = Collection.objects.filter(featured_product__isnull=True)
+    # query_set = Collection.objects.filter(featured_product__isnull=True)
 
     # Products with low inventory (less than 10)
-    #query_set = Product.objects.filter(inventory__lt=10)
+    # query_set = Product.objects.filter(inventory__lt=10)
 
     # Orders placed by customer with id = 1
-    #query_set = Order.objects.filter(customer__id=1)
+    # query_set = Order.objects.filter(customer__id=1)
 
     # Order items for products in collection 3
-    #query_set = OrderItem.objects.filter(Product.objects.filter(collection__id=3))
+    query_set = OrderItem.objects.filter(product__collection__id=3)
 
     return render(request, 'hello.html', {'name': 'John', 'results': list(query_set)})
