3b639c470cdec88efdfbde46d7cf125c2f025155
endisl <endiluamba@gmail.com>
2022-08-10 23:48:53 -0300

    store

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Collection, Customer, Order, OrderItem, Product
 
 
 def say_hello(request):
-    query_set = Product.objects.filter(unit_price__range=(20, 30))
+    # Customers with .com accounts
+    query_set = Customer.objects.filter(email__contains='.com')
 
-    return render(request, 'hello.html', {'name': 'John', 'products': list(query_set)})
+    # Collections that don't have a featured product
+    #query_set = Collection.objects.filter(featured_product__isnull=True)
+
+    # Products with low inventory (less than 10)
+    #query_set = Product.objects.filter(inventory__lt=10)
+
+    # Orders placed by customer with id = 1
+    #query_set = Order.objects.filter(customer__id=1)
+
+    # Order items for products in collection 3
+    #query_set = OrderItem.objects.filter(Product.objects.filter(collection__id=3))
+
+    return render(request, 'hello.html', {'name': 'John', 'results': list(query_set)})
