eac815274f13a42123fec83ca0efede4bb8e460a
endisl <endiluamba@gmail.com>
2022-08-23 01:08:16 -0300

    store

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 from store.models import Product, Collection
 
 
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
@@ -10,8 +15,14 @@ class ProductSerializer(serializers.Serializer):
         max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
-    collection = serializers.PrimaryKeyRelatedField(
-        queryset=Collection.objects.all()
+    # collection = serializers.PrimaryKeyRelatedField(
+    #    queryset=Collection.objects.all()
+    # )
+    #collection = serializers.StringRelatedField()
+    #collection = CollectionSerializer()
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(),
+        view_name='collection-detail',
     )
 
     def calculate_tax(self, product: Product):
