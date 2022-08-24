ceae2a16346da132dcd25927b68764b5a439969e
endisl <endiluamba@gmail.com>
2022-08-23 21:01:53 -0300

    store

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from itertools import product
 from rest_framework import serializers
 from store.models import Product, Collection
 
@@ -12,7 +13,8 @@ class CollectionSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory',
+                  'unit_price', 'price_with_tax', 'collection']
 
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
