9d97e3517988447265dd0942e21414d9099dc917
endisl <endiluamba@gmail.com>
2022-08-28 00:20:19 -0300

    store

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,17 +1,23 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from store.filters import ProductFilter
+
+from .filters import ProductFilter
 from .models import OrderItem, Product, Collection, Review
+from .pagination import DefaultPagination
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
