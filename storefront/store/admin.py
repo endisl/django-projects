361c7ec0394ceb3da7ec41f9e3e5b64dde7013e9
endisl <endiluamba@gmail.com>
2022-09-15 01:22:38 -0300

    store

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -19,6 +19,16 @@ class InventoryFilter(admin.SimpleListFilter):
             return queryset.filter(inventory__lt=10)
 
 
+class ProductImageInline(admin.TabularInline):
+    model = models.ProductImage
+    readonly_fields = ['thumbnail']
+
+    def thumbnail(self, instance):
+        if instance.image.name != '':
+            return format_html(f'<img src="{instance.image.url}" class="thumbnail" />')
+        return ''
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     autocomplete_fields = ['collection']
@@ -29,6 +39,7 @@ class ProductAdmin(admin.ModelAdmin):
     #exclude = ['promotions']
     #readonly_fields = ['title']
     actions = ['clear_inventory']
+    inlines = [ProductImageInline]
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
@@ -54,6 +65,11 @@ class ProductAdmin(admin.ModelAdmin):
             f'{updated_count} products were successfully updated',
             # messages.ERROR
         )
+
+    class Media:
+        css = {
+            'all': ['store/styles.css']
+        }
 
 
 @admin.register(models.Customer)
