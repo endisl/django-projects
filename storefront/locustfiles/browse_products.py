ee68ab8eae70863d58ba72ed205478a5c8248e6b
endisl <endiluamba@gmail.com>
2022-09-18 16:40:49 -0300

    store

--- storefront/locustfiles/browse_products.py
+++ storefront/locustfiles/browse_products.py
@@ -7,21 +7,21 @@ class WebsiteUser(HttpUser):
 
     @task(2)
     def view_products(self):
-        print('View products')
+        #print('View products')
         collection_id = randint(2, 6)
         self.client.get(
             f'/store/products/?collection_id={collection_id}', name='store/products')
 
     @task(4)
     def view_product(self):
-        print('View product details')
+        #print('View product details')
         product_id = randint(1, 1000)
         self.client.get(
             f'/store/products/{product_id}', name='store/products/:id')
 
     @task(1)
     def add_to_cart(self):
-        print('Add to cart')
+        #print('Add to cart')
         product_id = randint(1, 10)
         self.client.post(f'/store/carts/{self.cart_id}/items/',
                          name='/store/carts/items',
