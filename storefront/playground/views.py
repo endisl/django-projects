c3d1a24985dd113e54f32a06e5fb7940ddf3de58
endisl <endiluamba@gmail.com>
2022-10-18 18:39:51 -0300

    store

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
+import logging
 import requests
+
 #from .tasks import notify_customers
 #from templated_mail.mail import BaseEmailMessage
 #from django.core.mail import EmailMessage, BadHeaderError
@@ -17,13 +19,19 @@ import requests
 #from store.models import Collection, Customer, Order, OrderItem, Product
 #from tags.models import TaggedItem
 
+logger = logging.getLogger(__name__)
+
 
 class HelloView(APIView):
-    @method_decorator(cache_page(5 * 60))
+    # @method_decorator(cache_page(5 * 60))
     def get(self, request):
-        response = requests.get('https://httpbin.org/delay/2')
-        data = response.json()
-
+        try:
+            logger.info('Calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info('Received the response')
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical('httpbin is offline')
         return render(request, 'hello.html', {'name': 'John'})
 
 # @transaction.atomic()
