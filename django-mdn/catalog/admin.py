47c35e6738dac140a027f61b77eb8f861ab834cb
endisl <endiluamba@gmail.com>
2022-10-26 22:37:54 -0300

    mdn

--- django-mdn/catalog/admin.py
+++ django-mdn/catalog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, BookInstance, Genre, Language
+
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
