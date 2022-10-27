7e946c4059a637a0bca383138a8e9fe4c6cae61d
endisl <endiluamba@gmail.com>
2022-10-26 23:35:01 -0300

    mdn

--- django-mdn/catalog/admin.py
+++ django-mdn/catalog/admin.py
@@ -6,18 +6,40 @@ admin.site.register(Genre)
 admin.site.register(Language)
 
 
-class AuthorAdmin(admin.ModelAdmin):
-    pass
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+class BookInstaceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstaceInline]
 
 
-@admin.register(BookInstance)
-class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
 
 
-admin.site.register(Author, AuthorAdmin)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
