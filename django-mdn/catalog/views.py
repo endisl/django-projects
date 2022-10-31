6306b8e523dc8d898d39c11515fe5ba3ceb403ca
endisl <endiluamba@gmail.com>
2022-10-30 23:51:42 -0300

    mdn

--- django-mdn/catalog/views.py
+++ django-mdn/catalog/views.py
@@ -47,3 +47,11 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
