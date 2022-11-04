be6b7d2b96fb4687389fe6b0279125b9a6240d9e
endisl <endiluamba@gmail.com>
2022-11-03 23:53:32 -0300

    mdn

--- django-mdn/catalog/models.py
+++ django-mdn/catalog/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from datetime import date
 import uuid
 
 
@@ -59,6 +61,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -72,6 +76,11 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
     def __str__(self):
         """String for representing the BookInstance object"""
