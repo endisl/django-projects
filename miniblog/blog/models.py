bd5db3df6f72fd8d2aacb585456834b9e4710f60
endisl <endiluamba@gmail.com>
2022-12-01 00:09:15 -0300

    blog

--- miniblog/blog/models.py
+++ miniblog/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Blog(models.Model):
+    post_date = models.DateField()
+    author = models.ForeignKey('Blogger', on_delete=models.SET_NULL, null=True)
+    description = models.TextField(
+        max_length=1000, help_text="Enter the description of this blog.")
+
+
+class Blogger(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(
+        max_length=1000, help_text="Enter the bio of this blogger.")
