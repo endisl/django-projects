30b8ab39a9e685b804a9fb5acb4ff0db57449aac
endisl <endiluamba@gmail.com>
2022-12-20 22:44:47 -0300

    blog

--- miniblog/blog/tests/test_models.py
+++ miniblog/blog/tests/test_models.py
@@ -8,22 +8,34 @@ class BloggerModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        pass
+        user1 = User.objects.create_user(
+            username='so-and-so', password='12345')
+        user1.save()
+        Blogger.objects.create(user=user1, bio='my bio')
 
     def test_get_absolute_url(self):
-        pass
+        blogger = Blogger.objects.get(id=1)
+        self.assertEqual(blogger.get_absolute_url(), '/blog/blogger/1')
 
     def test_user_label(self):
-        pass
+        blogger = Blogger.objects.get(id=1)
+        field_label = blogger._meta.get_field('user').verbose_name
+        self.assertEqual(field_label, 'user')
 
     def test_bio_label(self):
-        pass
+        blogger = Blogger.objects.get(id=1)
+        field_label = blogger._meta.get_field('bio').verbose_name
+        self.assertEqual(field_label, 'bio')
 
     def test_bio_max_length(self):
-        pass
+        blogger = Blogger.objects.get(id=1)
+        max_length = blogger._meta.get_field('bio').max_length
+        self.assertEqual(max_length, 500)
 
     def test_object_name(self):
-        pass
+        blogger = Blogger.objects.get(id=1)
+        expected_object_name = blogger.user.username
+        self.assertEqual(expected_object_name, str(blogger))
 
 
 class BlogModelTest(TestCase):
